7ed2c15d102bc5bcc8f5850cbe832b2e3d9470c2
Irina Skopetskaya <sairina2704@gmail.com>
2022-08-25 23:25:39 +0300

    addmongodb

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,21 +1,19 @@
-const contactsOperation = require("../../models/contacts");
+const { Contact } = require("../../models");
+const { NotFound } = require("http-errors");
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsOperation.getContactById(contactId);
-  if (result) {
-    return res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } else {
-    const error = new Error(`Contact with id ${contactId} not found`);
-    error.status = 404;
-    throw error;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw new NotFound(`Contact with id ${contactId} not found`);
   }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
 
 module.exports = getById;
